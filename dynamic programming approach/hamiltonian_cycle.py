d302a9fd0fcab5f7762da5305f8510a79eb766d6
draszec <draszec@gmail.com>
2024-04-03 08:51:22 +0200

    Make (visited nodes, last visited node) pair add to a set instead to a list. This prevents duplicates and significantly reduces time of execution and probably memory usage. Delete redundant adjacency list example. Rename adjacency_list in generate_graph() to new_adjacency_list to avoid the same name as for the global variable.

--- dynamic programming approach/hamiltonian_cycle.py
+++ dynamic programming approach/hamiltonian_cycle.py
@@ -1,25 +1,16 @@
 import random
 import time
 
-adjacency_list = \
-    (
-        (1, 2, 5),
-        (0, 3, 5),
-        (0, 4),
-        (1, 4, 6),
-        (2, 3, 6),
-        (1, 0, 6),
-        (3, 4, 5),
-    )
+adjacency_list: tuple[tuple[int]]
 
 
-def generate_graph(size: int, number_of_edges: int):
+def generate_graph(size: int, number_of_edges: int) -> tuple[tuple[int]]:
     assert size > 0 and number_of_edges >= 0
-    assert number_of_edges <= size * (size - 1) / 2
+    # assert number_of_edges <= size * (size - 1) / 2
 
-    adjacency_list = []
+    new_adjacency_list = []
     for i in range(size):
-        adjacency_list.append([])
+        new_adjacency_list.append([])
 
     for i in range(number_of_edges):
         first_node = random.randint(0, size - 1)
@@ -28,26 +19,28 @@ def generate_graph(size: int, number_of_edges: int):
             first_node = random.randint(0, size - 1)
             second_node = random.randint(0, size - 1)
 
-        adjacency_list[first_node].append(second_node)
-        adjacency_list[second_node].append(first_node)
+        new_adjacency_list[first_node].append(second_node)
+        new_adjacency_list[second_node].append(first_node)
 
-    for i in range(len(adjacency_list)):
-        adjacency_list[i] = tuple(set(adjacency_list[i]))
+    for i in range(len(new_adjacency_list)):
+        new_adjacency_list[i] = tuple(set(new_adjacency_list[i]))
 
-    return tuple(adjacency_list)
+    return tuple(new_adjacency_list)
+
 
 def hamiltonian_cycle() -> bool:
-
-    hamiltonian_list = []
+    hamiltonian_list: list[set[tuple[frozenset, int]]] = []
     for i in range(len(adjacency_list)):
-        hamiltonian_list.append([])
+        # t = set()
+        # t = t.add(tuple((set, int)))
+        hamiltonian_list.append(set())
 
     starting_node = 0
     for i in range(len(adjacency_list)):
         if len(adjacency_list[i]) > len(adjacency_list[starting_node]):
             starting_node = i
 
-    hamiltonian_list[0].append(({starting_node}, starting_node))
+    hamiltonian_list[0].add((frozenset({starting_node}), starting_node))
 
     # Number of iterations = number of vertices - 1
     for i in range(len(adjacency_list) - 1):
@@ -55,8 +48,8 @@ def hamiltonian_cycle() -> bool:
             for adjacent_node in adjacency_list[last_node]:
                 if adjacent_node not in path:
                     new_path = {adjacent_node}
-                    new_path = new_path.union(path)
-                    hamiltonian_list[i + 1].append((new_path, adjacent_node))
+                    new_path = frozenset(new_path.union(path))
+                    hamiltonian_list[i + 1].add((new_path, adjacent_node))
 
     for _, last_node in hamiltonian_list[-1]:
         if starting_node in adjacency_list[last_node]:
@@ -75,3 +68,4 @@ if __name__ == "__main__":
 
     # print(adjacency_list)
     print(cycle)
+
